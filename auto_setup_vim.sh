74d987263938b98db9ea0ff97b661fee58471141
liangfeng <liang.feng98 AT gmail DOT com>
2014-04-14 15:08:06 +0800

    Enable lua support

--- auto_setup_vim.sh
+++ auto_setup_vim.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/env bash
 
+# Install python-devel and lua-devel
+sudo yum -y install python-devel > /dev/null
+sudo yum -y install lua-devel > /dev/null
+
 # Fetch vim source code and build latest vim.
 rm -rf ./vim/
 git clone https://github.com/b4winckler/vim.git vim
 cd ./vim/src/
 make distclean
-./configure --with-features=huge --disable-selinux --enable-cscope --enable-multibyte --without-x --enable-pythoninterp --with-compiledby='Liang Feng <liang.feng98 AT gmail DOT com>'
+./configure --with-features=huge --disable-selinux --enable-cscope --enable-multibyte --without-x --enable-pythoninterp --enable-luainterp --with-compiledby='Liang Feng <liang.feng98 AT gmail DOT com>'
 make
 sudo make install
 
