782aa74daaff169bc39d471bd5612bb70f333038
liangfeng <liang.feng98 AT gmail DOT com>
2014-04-30 23:38:01 +0800

    this script can run from non-src dir now.

--- build_vim.bat
+++ build_vim.bat
@@ -5,8 +5,12 @@
 :: following vars only available in this bat file
 setlocal
 
-set SDK_INCLUDE_DIR=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
+set CURDIR=%~dp0
+set VIM_SRC=%CURDIR%..\vim_src
+set DST="C:\Program Files (x86)\Vim\vim74"
 
+set SDK_INCLUDE_DIR=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
+cd %VIM_SRC%\src
 :: build GUI version
 nmake -f Make_mvc.mak FEATURES=HUGE MBYTE=yes CSCOPE=yes SNIFF=no NETBEANS=no CPUNR=pentium4 DEBUG=no MAP=yes PYTHON=C:\Python27 PYTHON_VER=27 DYNAMIC_PYTHON=yes LUA=C:\LUA LUA_VER=52 DYNAMIC_LUA=yes USERNAME="Liang Feng <liang.feng98 AT gmail DOT com>" USERDOMAIN=China GUI=yes OLE=yes IME=yes GIME=yes %*
 
@@ -16,15 +20,12 @@ nmake -f Make_mvc.mak FEATURES=HUGE MBYTE=yes CSCOPE=yes SNIFF=no NETBEANS=no CP
 if "%1" == "clean" goto :End
 
 :: Copy any new Vim exe & runtime files to current install.
-set SRC="C:\Users\liangfeng\personal\projects\download\vim_src"
-set DST="C:\Program Files (x86)\Vim\vim74"
-
-xcopy %SRC%\runtime %DST% /D /E /H /I /Y
-xcopy %SRC%\src\xxd\xxd.exe %DST%\* /D /Y
-xcopy %SRC%\src\*.exe %DST%\* /D /Y
-xcopy %SRC%\src\*.pdb %DST%\* /D /Y
-xcopy %SRC%\src\*.map %DST%\* /D /Y
-REM  xcopy %SRC%\src\GvimExt\gvimext.dll %DST%\* /D /Y
+xcopy %VIM_SRC%\runtime %DST% /D /E /H /I /Y
+xcopy %VIM_SRC%\src\xxd\xxd.exe %DST%\* /D /Y
+xcopy %VIM_SRC%\src\*.exe %DST%\* /D /Y
+xcopy %VIM_SRC%\src\*.pdb %DST%\* /D /Y
+xcopy %VIM_SRC%\src\*.map %DST%\* /D /Y
+REM  xcopy %VIM_SRC%\src\GvimExt\gvimext.dll %DST%\* /D /Y
 
 :End
 
