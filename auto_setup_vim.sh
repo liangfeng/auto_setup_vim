02359563faa0d7776a96417f9a8d13da262134ee
liangfeng <liang.feng98 AT gmail DOT com>
2014-04-30 17:48:39 +0800

    fix error.

--- auto_setup_vim.sh
+++ auto_setup_vim.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env bash
 
-VIM_SRC=vim_src
+VIM_SRC=../vim_src
 
 # Install python-devel and lua-devel
 sudo yum -y install python-devel > /dev/null
 sudo yum -y install lua-devel > /dev/null
 
 # Fetch vim source code and build latest vim.
-if [ -d ../$VIM_SRC/ ]
+if [ -d $VIM_SRC/ ]; then
     git pull
-elif
+else
     git clone https://github.com/b4winckler/vim.git $VIM_SRC
 fi
 
